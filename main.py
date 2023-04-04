141fb1d565cca406f69e6a3f7c4dac7bed22a4de
Continy <satanama.ring@gmail.com>
2023-04-04 13:02:05 +0800

    main control program for unity visual env

---
+++ main.py
@@ -0,0 +1,239 @@
+import socket
+import json
+import threading
+import time
+from rrt_circle import RRT
+import matplotlib.pyplot as plt
+import math
+
+Host = '127.0.0.1'  #IP地址
+
+Port_GPS = 60100  #GPS端口
+Port_IMU = 60101  #IMU端口
+Port_Buoy = 60103  #浮标端口
+
+Port_Control = 60199  #控制端口
+Port_Path = 60198  #路径输出端口
+
+socket_GPS = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # 创建UDP套接字
+socket_IMU = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socket_Buoy = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+socket_Control = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socket_Path = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+socket_GPS.bind((Host, Port_GPS))  #端口绑定
+socket_IMU.bind((Host, Port_IMU))
+socket_Buoy.bind((Host, Port_Buoy))
+
+longitude = None  #经度
+latitude = None  #纬度
+
+pitch = None  #俯仰角
+roll = None  #翻滚角
+yaw = None  #偏航角
+velocity = None  #速度
+
+buoy_list = None  #浮标位置
+path = None  #路径
+
+
+# GPS信息接收函数
+def GPS_receive():
+    global longitude, latitude
+    while True:
+        data, _ = socket_GPS.recvfrom(1024)
+        data_json = json.loads(data.decode('utf-8'))
+        longitude = data_json["longitude"]
+        latitude = data_json["latitude"]
+
+
+# IMU信息接收函数
+def IMU_receive():
+    global pitch, roll, yaw, velocity
+    while True:
+        data, _ = socket_IMU.recvfrom(1024)
+        data_json = json.loads(data.decode('utf-8'))
+        pitch = data_json["pitch"]
+        roll = data_json["roll"]
+        yaw = data_json["yaw"]
+        velocity = data_json["velocity"]
+
+
+# 浮标位置接收函数
+def Buoy_receive():
+    global buoy_list
+    while True:
+        data, _ = socket_Buoy.recvfrom(1024)
+        data_json = json.loads(data.decode('utf-8'))
+        buoy_list = data_json['buoys']
+
+
+# 发送路径
+def Path_send(path_points):
+    path = {}
+    path["points"] = path_points
+    path = json.dumps(path)
+    path = path.encode('utf-8')
+    socket_Path.sendto(path, (Host, Port_Path))
+
+
+# 发送控制信号，throttle为油门，steer为方向，范围均为 -1 ~ 1
+def Control_send(throttle, steer):
+    control_cmd = {}
+    control_cmd["throttle"] = throttle
+    control_cmd["steer"] = steer
+    control_cmd = json.dumps(control_cmd)
+    control_cmd = control_cmd.encode('utf-8')
+    socket_Control.sendto(control_cmd, (Host, Port_Control))
+
+
+#以起点为原点，计算目标点的相对坐标,单位为米
+def get_relative_position(longitude, latitude, target_longitude,
+                          target_latitude):
+    #x为东西方向，y为南北方向，单位为米
+    x = math.cos(
+        latitude * math.pi / 180) * (target_longitude - longitude) * 111000
+    y = (target_latitude - latitude) * 111000
+    return [x, y]
+
+
+#已知目标点的相对坐标，计算目标点的绝对坐标
+def get_absolute_position(longitude, latitude, x, y):
+    #x为东西方向，y为南北方向，单位为米d
+    target_longitude = longitude + x / 111000 / math.cos(
+        latitude * math.pi / 180)
+    target_latitude = latitude + y / 111000
+    return [target_longitude, target_latitude]
+
+
+def get_path():
+    if longitude is None or latitude is None or buoy_list is None:
+        return
+    else:
+        return path()
+
+
+def path():
+    global path
+    global buoy_list
+    wps84_obstacles = buoy_list
+    wps84_begin = [121.438074312978, 31.0286316336127]
+    wps84_end = [121.438954507803, 31.0280814275016]
+    begin = get_relative_position(wps84_begin[0], wps84_begin[1],
+                                  wps84_begin[0], wps84_begin[1])
+    end = get_relative_position(wps84_begin[0], wps84_begin[1], wps84_end[0],
+                                wps84_end[1])
+    # with open('obstacles.json', 'r') as f:
+    #     wps84_obstacles = json.load(f)
+    obstacles = []
+    for obstacle in wps84_obstacles:
+        [x, y] = get_relative_position(wps84_begin[0], wps84_begin[1],
+                                       obstacle['longitude'],
+                                       obstacle['latitude'])
+        obstacles.append([x, y, 2.5])
+    #坐标转换
+    #print(obstacles)
+    # plt.figure()
+    # for obstacle in obstacles:
+    #     circle = plt.Circle((obstacle[0], obstacle[1]), obstacle[2])
+    #     plt.gca().add_patch(circle)
+    # plt.scatter(begin[0], begin[1], c='r')
+    # plt.scatter(end[0], end[1], c='g')
+
+    boundary = [-35, 58, -35, 10]
+    #draw boundary
+    # plt.plot([boundary[0], boundary[1]], [boundary[2], boundary[2]], c='k')
+    # plt.plot([boundary[0], boundary[1]], [boundary[3], boundary[3]], c='k')
+    # plt.plot([boundary[0], boundary[0]], [boundary[2], boundary[3]], c='k')
+    # plt.plot([boundary[1], boundary[1]], [boundary[2], boundary[3]], c='k')
+    step_size = 1
+    max_iter = 1000
+    rrt = RRT(begin, end, obstacles, step_size, max_iter, boundary)
+    rrt.build_rrt()
+    rrt.prune(rrt.goal)
+    rrt.get_path()
+    rrt.get_control_points(mode='default')
+    path_points = []
+    t = 0
+    for line in rrt.path:
+        if t == 0:
+            path_points.append([line[0].x, line[0].y])
+            path_points.append([line[1].x, line[1].y])
+            t = 1
+            continue
+        path_points.append([line[1].x, line[1].y])
+    wps84_path = []  #json格式
+    for point in path_points:
+        [longitude,
+         latitude] = get_absolute_position(wps84_begin[0], wps84_begin[1],
+                                           point[0], point[1])
+        wps84_path.append({'longitude': longitude, 'latitude': latitude})
+    with open('path.json', 'w') as f:
+        json.dump(wps84_path, f)
+
+    total_curve = rrt.bezier()
+    wps84_curve = []  #json格式
+    for curve in total_curve:
+        x = [point.x for point in curve]
+        y = [point.y for point in curve]
+        plt.plot(x, y, 'r-')
+    for curve in total_curve:
+        for point in curve:
+            [longitude,
+             latitude] = get_absolute_position(wps84_begin[0], wps84_begin[1],
+                                               point.x, point.y)
+            wps84_curve.append({'longitude': longitude, 'latitude': latitude})
+    with open('curve.json', 'w') as f:
+        json.dump(wps84_curve, f)
+    #rrt.plot()
+    return wps84_path
+
+
+# 控制算法
+def Model():
+
+    while True:
+
+        if longitude == None or latitude == None or pitch == None or roll == None or yaw == None:
+
+            # 如果没有收到信息，就跳出这个循环
+            time.sleep(0.1)
+            continue
+
+        print("longitude:", longitude)
+        print("latitude:", latitude)
+
+        print("pitch:", pitch)
+        print("roll:", roll)
+        print("yaw:", yaw)
+
+        print("buoy:", buoy_list)
+
+        # path = [{'longitude': 121.43813605886, 'latitude': 31.0285482382593}, {'longitude': 121.438256509255, 'latitude': 31.0284508268229},\
+        #         {'longitude': 121.438370675377, 'latitude': 31.0283723565488}, {'longitude': 121.438253367199, 'latitude': 31.0283146312557},\
+        #         {'longitude': 121.438463893444, 'latitude': 31.0284643561852}]
+        path = get_path()
+        try:
+            Path_send(path)
+        except:
+            pass
+
+        Control_send(0, 0)
+        time.sleep(2)
+
+
+if __name__ == "__main__":
+
+    #多线程接收
+    thread_GPS = threading.Thread(target=GPS_receive, daemon=True)
+    thread_IMU = threading.Thread(target=IMU_receive, daemon=True)
+    thread_Buoy = threading.Thread(target=Buoy_receive, daemon=True)
+    thread_Path = threading.Thread(target=get_path, daemon=True)
+    thread_GPS.start()
+    thread_IMU.start()
+    thread_Buoy.start()
+    thread_Path.start()
+
+    #控制程序
+    Model()
--- rrt_circle.py
+++ rrt_circle.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import math
+from scipy import interpolate
 
 
 class Node:
@@ -13,7 +15,7 @@ class Node:
 
 class RRT:
 
-    def __init__(self, start, goal, obstacles, step_size, max_iter):
+    def __init__(self, start, goal, obstacles, step_size, max_iter, boundary):
         self.start = Node(start[0], start[1])
         self.goal = Node(goal[0], goal[1])
         self.obstacles = obstacles
@@ -21,10 +23,25 @@ class RRT:
         self.max_iter = max_iter
         self.nodes = [self.start]
         self.edges = []
+        self.origin_path = []
+        self.assist_control_points = []
+        self.control_points = []
+        self.path = []
+        self.boundary = boundary
+
+    def cal_distance(self, node1, node2):  # 参数形式 point：[x,y]
+        if node2.__class__ == Node:
+            distance = math.sqrt((node1.x - node2.x)**2 +
+                                 (node1.y - node2.y)**2)
+            return distance
+        else:
+            distance = math.sqrt((node1.x - node2[0])**2 +
+                                 (node1.y - node2[1])**2)
+            return distance
 
     def random_sample(self):
-        x = random.uniform(0, 10)
-        y = random.uniform(0, 10)
+        x = random.uniform(self.boundary[0], self.boundary[1])
+        y = random.uniform(self.boundary[2], self.boundary[3])
         return Node(x, y)
 
     def find_nearest_node(self, node):
@@ -37,27 +54,35 @@ class RRT:
                 nearest_node = n
         return nearest_node
 
-    def is_collision_free(self, node1, node2):
-        for obs in self.obstacles:
-            x_obs, y_obs, r_obs = obs
-            x1, y1 = node1.x, node1.y
-            x2, y2 = node2.x, node2.y
-            m = (y2 - y1) / (x2 - x1)
-            c = y1 - m * x1
-            x = np.linspace(x1, x2, num=50)
-            y = m * x + c
-            d = (x - x_obs)**2 + (y - y_obs)**2
-            if any(d < 1):
+    def check_collision(
+            self, node1, node2
+    ):  # 参数形式 point：[x,y]; obstacle: [x,y,r] 设障碍物为圆形，x,y为圆心坐标，r为圆的半径
+        #求两向量叉积
+        for obstacle in self.obstacles:
+            p1 = [node1.x - obstacle[0], node1.y - obstacle[1]]
+            p2 = [node2.x - obstacle[0], node2.y - obstacle[1]]
+            #三角形面积的两倍
+            area = abs(p1[0] * p2[1] - p1[1] * p2[0])
+            #三角形底边长度
+            base = self.cal_distance(node1, node2)
+            #求高
+            height = area / base
+            #三角形较长边长
+            long_side = max(self.cal_distance(node1, obstacle),
+                            self.cal_distance(node2, obstacle))
+            #三角形较短边长
+            short_side = min(self.cal_distance(node1, obstacle),
+                             self.cal_distance(node2, obstacle))
+            #求垂足到较远点的距离
+            distance = math.sqrt(long_side**2 - height**2)
+
+            if distance > base and short_side > obstacle[2]:
+                continue
+            elif height > obstacle[2]:
+                continue
+            else:
                 return False
         return True
-
-    """ Here is the explanation for the code above:
-1. We create a random sample and find the nearest node to the sample. 
-2. We then calculate the angle between the random sample and the nearest node.
-3. We create a new node at a distance of step_size from the nearest node in the direction of the random sample.
-4. We check if the new node is collision free.
-5. If it is, we add it to the tree and then check if we can connect it to the goal.
-6. If we can, we add the goal to the tree and break out of the loop. """
 
     def build_rrt(self):
         for i in range(self.max_iter):
@@ -67,32 +92,143 @@ class RRT:
                                sample.x - nearest_node.x)
             new_node = Node(nearest_node.x + self.step_size * np.cos(theta),
                             nearest_node.y + self.step_size * np.sin(theta))
-            if self.is_collision_free(nearest_node, new_node):
+            #判断是否碰撞
+            if self.check_collision(nearest_node, new_node):  #如果不碰撞
                 self.nodes.append(new_node)
                 new_node.parent = nearest_node
                 self.edges.append((nearest_node, new_node))
-                if self.is_collision_free(new_node, self.goal):
+                if self.check_collision(new_node, self.goal):  #如果到达终点
                     self.nodes.append(self.goal)
                     self.goal.parent = new_node
                     self.edges.append((new_node, self.goal))
                     break
+        node = self.goal
+        while True:
+            if node.parent is not None:
+                self.origin_path.append((node.parent, node))
+                node = node.parent
+            else:
+                self.origin_path.append((self.start, node))
+                break
+
+    #获取某一节点所有的上级节点
+    def get_parents(self, node):
+        parents = []
+        generation = 1
+        while node.parent is not None:
+            parents.append([node.parent, generation])
+            node = node.parent
+            generation += 1
+        parents.reverse()
+        return parents
+
+    def prune(self, node, mode=float('inf')):
+
+        if node.parent is None:
+            return
+        else:
+            for point in self.get_parents(node):
+                if point[1] > mode:
+                    continue
+                if self.check_collision(point[0], node):
+                    node.parent = point[0]
+                    break
+            self.prune(node.parent, mode)
+
+    def get_path(self):
+        self.path = []
+        node = self.goal
+        while True:
+            if node.parent is not None:
+                self.path.append((node.parent, node))
+                node = node.parent
+            else:
+                self.path.append((self.start, node))
+                break
+
+    #点到直线的距离
+    def point_to_line(self, node1, node2, node3):
+        a = node2.y - node1.y
+        b = node1.x - node2.x
+        c = node2.x * node1.y - node1.x * node2.y
+        return abs(a * node3.x + b * node3.y + c) / math.sqrt(a**2 + b**2)
+
+    #构建贝塞尔曲线所需的控制点
+    def get_control_points(self, mode='default'):
+        if mode == 'default':
+            for i in range(0, len(self.origin_path) - 1):
+                if i == 0:
+                    self.control_points.append(self.origin_path[i][1])
+                    self.control_points.append(self.origin_path[i][0])
+                # elif i % 3 == 0:
+                #     k = (self.origin_path[i][1].y - self.origin_path[i][0].y) / (
+                #         self.origin_path[i][1].x - self.origin_path[i][0].x)
+                #     theta = math.atan(k)
+                #     #辅助点x坐标
+                #     x = self.origin_path[i][1].x + self.step_size * math.cos(
+                #         theta) / 2
+                #     #辅助点y坐标
+                #     y = self.origin_path[i][1].y + self.step_size * math.sin(
+                #         theta) / 2
+                #     self.assist_control_points.append(Node(x, y))
+                #     self.control_points.append(Node(x, y))
+                #     self.control_points.append(self.origin_path[i][1])
+                else:
+                    self.control_points.append(self.origin_path[i][0])
+        elif mode == 'max':
+            for i in range(0, len(self.path) - 1):
+                if i == 0:
+                    self.control_points.append(self.path[i][1])
+                    self.control_points.append(self.path[i][0])
+                else:
+                    self.control_points.append(self.path[i][0])
+
+    #绘制贝塞尔曲线
+    def bezier(self):
+        total_curve = []
+        for i in range(0, len(self.control_points) - 3, 3):
+            curve = []
+            for t in np.arange(0, 1, 0.01):
+                x = (1 - t)**3 * self.control_points[i].x + 3 * t * (
+                    1 - t)**2 * self.control_points[i + 1].x + 3 * t**2 * (
+                        1 - t) * self.control_points[
+                            i + 2].x + t**3 * self.control_points[i + 3].x
+                y = (1 - t)**3 * self.control_points[i].y + 3 * t * (
+                    1 - t)**2 * self.control_points[i + 1].y + 3 * t**2 * (
+                        1 - t) * self.control_points[
+                            i + 2].y + t**3 * self.control_points[i + 3].y
+                curve.append(Node(x, y))
+            total_curve.append(curve)
+        return total_curve
 
     def plot(self):
-        plt.figure()
-        plt.plot(self.start.x, self.start.y, 'go')
-        plt.plot(self.goal.x, self.goal.y, 'ro')
+        # plt.figure()
+        # plt.plot(self.start.x, self.start.y, 'go')
+        # plt.plot(self.goal.x, self.goal.y, 'ro')
         node = self.goal
         for edge in self.edges:
             x = [edge[0].x, edge[1].x]
             y = [edge[0].y, edge[1].y]
             plt.plot(x, y, 'b-')
-        # while True:
-        #     if node.parent is not None:
-        #         plt.plot([node.x, node.parent.x], [node.y, node.parent.y],
-        #                  'b-')
-        #         node = node.parent
-        #     else:
-        #         break
+        for edge in self.origin_path:
+            x = [edge[0].x, edge[1].x]
+            y = [edge[0].y, edge[1].y]
+            plt.plot(x, y, 'y-')
+        for line in self.path:
+            x = [line[0].x, line[1].x]
+            y = [line[0].y, line[1].y]
+            plt.plot(x, y, 'g-')
+        # for point in self.control_points:
+        #     plt.scatter(point.x, point.y, c='k')
+        # for point in self.assist_control_points:
+        #     plt.scatter(point.x, point.y, c='r')
+        # #绘制样条曲线
+        # total_curve = self.bezier()
+        # for curve in total_curve:
+        #     x = [point.x for point in curve]
+        #     y = [point.y for point in curve]
+        #     plt.plot(x, y, 'r-')
+
         for obs in self.obstacles:
             x, y, r = obs
             circle = plt.Circle((x, y), r, color='k')
@@ -101,12 +237,21 @@ class RRT:
         plt.show()
 
 
-start = [1, 1]
-goal = [9, 9]
-obstacles = [[5, 5, 1], [1, 5, 1], [4, 2, 1], [1, 9, 1], [1, 3, 1]]
-step_size = 0.3
-max_iter = 10000
+if __name__ == '__main__':
+    boundary = [0, 100, 0, 100]
+    start = [10, 10]
+    goal = [90, 90]
+    obstacles = [[55, 60, 10], [100, 60, 15], [40, 30, 18], [90, 20, 15],
+                 [20, 40, 10], [60, 100, 15], [65, 45, 10], [85, 45, 10],
+                 [10, 40, 10], [76, 77, 13]]
+    # obstacles = [[55, 60, 10], [100, 60, 15], [40, 30, 18], [90, 20, 15],
+    #              [20, 40, 10], [60, 100, 15], [65, 45, 10]]
+    step_size = 4
+    max_iter = 10000
 
-rrt = RRT(start, goal, obstacles, step_size, max_iter)
-rrt.build_rrt()
-rrt.plot()
+    rrt = RRT(start, goal, obstacles, step_size, max_iter, boundary)
+    rrt.build_rrt()
+    rrt.prune(rrt.goal, 3)
+    rrt.get_path()
+    rrt.get_control_points()
+    rrt.plot()
